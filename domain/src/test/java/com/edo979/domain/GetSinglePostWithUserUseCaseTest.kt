20eeac12930581780e82350cf45150dcfb1b34fc
edo979 <prezentabilan@gmail.com>
2025-02-26 12:56:05 +0100

    Edit UseCase for fetching single post and add test for it.

---
+++ domain/src/test/java/com/edo979/domain/GetSinglePostWithUserUseCaseTest.kt
@@ -0,0 +1,43 @@
+package com.edo979.domain
+
+import com.edo979.domain.entity.Post
+import com.edo979.domain.entity.PostWithUser
+import com.edo979.domain.entity.User
+import com.edo979.domain.repository.PostRepository
+import com.edo979.domain.repository.UserRepository
+import com.edo979.domain.usecase.GetSinglePostWithUserUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class GetSinglePostWithUserUseCaseTest {
+
+    private val postRepository = mock<PostRepository>()
+    private val userRepository = mock<UserRepository>()
+    private val useCase = GetSinglePostWithUserUseCase(
+        mock(),
+        postRepository,
+        userRepository
+    )
+
+    @Test
+    fun testProcess() = runTest {
+        val id = 1L
+        val user1 = User(id, "name1", "username1", "email1")
+        val post1 = Post(1L, userId = id, "title1", "body1")
+
+        whenever(postRepository.getPost(id)).thenReturn(flowOf(post1))
+        whenever(userRepository.getUser(id)).thenReturn(flowOf(user1))
+
+        val result = useCase.process(GetSinglePostWithUserUseCase.Request(id)).first()
+
+        Assert.assertEquals(
+            GetSinglePostWithUserUseCase.Response(PostWithUser(post1, user1)),
+            result
+        )
+    }
+}
