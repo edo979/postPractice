83e24758e7e9278373f07425ed6ab7ed75f11f60
edo979 <prezentabilan@gmail.com>
2025-03-03 21:17:01 +0100

    Add Test for Save favorite post use case.

---
+++ domain/src/test/java/com/edo979/domain/SaveFavoritePostUseCaseTest.kt
@@ -0,0 +1,30 @@
+package com.edo979.domain
+
+import com.edo979.domain.entity.Post
+import com.edo979.domain.repository.FavoritePostRepository
+import com.edo979.domain.usecase.favoritePost.SaveFavoritePostUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class SaveFavoritePostUseCaseTest {
+
+    private val favoritePostRepository = mock<FavoritePostRepository>()
+    private val useCase = SaveFavoritePostUseCase(mock(), favoritePostRepository)
+
+    @Test
+    fun testProcess() = runTest {
+        val post = Post(
+            id = 1L, userId = 1L, title = "Title", body = "Body"
+        )
+        whenever(favoritePostRepository.addPost(post)).thenReturn(flowOf(post))
+
+        val result = useCase.process(SaveFavoritePostUseCase.Request(post)).first()
+
+        Assert.assertEquals(SaveFavoritePostUseCase.Response, result)
+    }
+}
