0596cc4cc06dfb191cbcd8717af54ebfa9002e42
edo979 <prezentabilan@gmail.com>
2025-03-04 20:52:10 +0100

    Start testing data-local module

--- data-local/src/main/java/com/edo979/data_local/db/AppDatabase.kt
+++ data-local/src/main/java/com/edo979/data_local/db/AppDatabase.kt
@@ -1,11 +1,12 @@
 package com.edo979.data_local.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.edo979.data_local.db.post.PostDao
 import com.edo979.data_local.db.post.PostEntity
 
 @Database(entities = [PostEntity::class], version = 1, exportSchema = false)
-abstract class AppDatabase {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun postDao(): PostDao
 }
---
+++ data-local/src/test/java/com/edo979/data_local/LocalPostDataSourceImplTest.kt
@@ -0,0 +1,52 @@
+package com.edo979.data_local
+
+import com.edo979.data_local.db.post.PostDao
+import com.edo979.data_local.db.post.PostEntity
+import com.edo979.data_local.sources.LocalPostDataSourceImpl
+import com.edo979.domain.entity.Post
+import com.edo979.domain.entity.PostWithUser
+import com.edo979.domain.entity.User
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class LocalPostDataSourceImplTest {
+
+    private val postDao = mock<PostDao>()
+    private val localDataSource = LocalPostDataSourceImpl(postDao)
+    private val id = 1L
+    private val postWithUser = PostWithUser(
+        post = Post(
+            id = id, userId = 1L, title = "title", body = "body"
+        ), user = User(
+            id = 1L, name = "name", username = "username", email = "email"
+        )
+    )
+    private val postEntity = PostEntity(
+        id = id,
+        title = "title",
+        body = "body",
+        userId = 1L,
+        authorName = "name",
+        authorUsername = "username",
+        authorEmail = "email"
+    )
+
+    @Test
+    fun testGetPosts() = runTest {
+        whenever(postDao.getPosts()).thenReturn(flowOf(listOf(postEntity)))
+        val result = localDataSource.getPosts().first()
+        Assert.assertEquals(listOf(postWithUser), result)
+    }
+
+    @Test
+    fun testGetPostsWhenNoPosts() = runTest {
+        whenever(postDao.getPosts()).thenReturn(flowOf(emptyList()))
+        val result = localDataSource.getPosts().first()
+        Assert.assertEquals(emptyList<PostWithUser>(), result)
+    }
+}
