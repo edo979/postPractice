23433d5022413d71eb462cb70b51f183a087141e
edo979 <prezentabilan@gmail.com>
2025-03-11 10:02:05 +0100

    Refactor IdlingLocalPostRepository.kt

--- app/src/androidTest/java/com/edo979/postpractice/repository/IdlingLocalPostRepository.kt
+++ app/src/androidTest/java/com/edo979/postpractice/repository/IdlingLocalPostRepository.kt
@@ -5,6 +5,7 @@ import com.edo979.domain.repository.FavoritePostRepository
 import com.edo979.postpractice.idling.ComposeCountingIdlingResource
 import com.edo979.postpractice.idling.attachIdling
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class IdlingLocalPostRepository(
     private val localPostRepository: FavoritePostRepository,
@@ -17,10 +18,23 @@ class IdlingLocalPostRepository(
     override fun getPost(id: Long): Flow<PostWithUser?> =
         localPostRepository.getPost(id).attachIdling(countingIdlingResource)
 
-    override fun addPost(post: PostWithUser): Flow<Unit> =
-        localPostRepository.addPost(post).attachIdling(countingIdlingResource)
+    override fun addPost(post: PostWithUser): Flow<Unit> = flow {
+        try {
+            countingIdlingResource.increment()
+            localPostRepository.addPost(post)
+            emit(Unit)
+        } finally {
+            countingIdlingResource.decrement()
+        }
+    }
 
-    override fun deletePost(post: PostWithUser): Flow<Unit> =
-        localPostRepository.deletePost(post).attachIdling(countingIdlingResource)
-
+    override fun deletePost(post: PostWithUser): Flow<Unit> = flow {
+        try {
+            countingIdlingResource.increment()
+            localPostRepository.deletePost(post)
+            emit(Unit)
+        } finally {
+            countingIdlingResource.decrement()
+        }
+    }
 }
